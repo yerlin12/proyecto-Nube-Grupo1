1e556322cc52f5f034a38497cbfed693c2e7834b
Usuario <Usuario@DESKTOP-D3MPC3U>
2022-03-18 19:51:25 -0600

    Agregar archivos de proyecto.

---
+++ proyecto-Nube-Grupo1/Pages/Contacto.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace proyecto_Nube_Grupo1.Pages
+{
+    public class ContactoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
